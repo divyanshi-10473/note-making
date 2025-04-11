afafa9e1a14aec230b6d4dda48e3566e37e8e55b
Divyanshi <divyanshi10473@gmail.com>
2025-04-11 11:00:07 +0530

    change

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config();
+
+
+mongoose.connect( process.env.MONGODB  
+).then(()=>console.log('MongoDb connected')).catch(error=> console.log(error))
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET','POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            'Express',
+            'Pragma'
+
+        ],
+        credentials: true
+    })
+);
+
+app.use(cookieParser())
+app.use(express.json());
+
+app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`))
+
